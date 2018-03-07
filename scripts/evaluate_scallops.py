e90b3dc77f4137d140f09c8e2a53ac49a9bb4f9f
Benjamin Woodward <bgwoodward@gmail.com>
2018-03-07 14:59:29 -0500

    Adding evaluation scripts

---
+++ scripts/evaluate_scallops.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+"""
+Copyright 2017-2018 Fizyr (https://fizyr.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
+TODO
+
+CSV Generator with proper image_min_side and image_max_side
+Run MC times
+Generate IOU and concatenation routine
+ - IOU calcs and Hungarian algorithm? Need to handle potential mismatch list size
+
+"""
+
+import keras
+import keras.preprocessing.image
+from keras_retinanet.preprocessing.csv_generator import CSVGenerator
+from keras_retinanet.models.resnet import custom_objects
+from keras_retinanet.utils.keras_version import check_keras_version
+
+import tensorflow as tf
+
+import argparse
+import os
+import numpy as np
+import pickle
+
+def get_session():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    return tf.Session(config=config)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Simple training script for COCO object detection.')
+    parser.add_argument('model', help='Path to RetinaNet model.')
+    parser.add_argument('test_path', help='Path to test images directory (ie. /tmp/COCO).')
+    parser.add_argument('class_file', help='Name of the class file to evaluate')
+    parser.add_argument('mean_image', help='Mean image file to subtract')
+    parser.add_argument('--gpu', help='Id of the GPU to use (as reported by nvidia-smi).')
+    parser.add_argument('--score-threshold', help='Threshold on score to filter detections with (defaults to 0.05).', default=0.05, type=float)
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # parse arguments
+    args = parse_args()
+
+    # make sure keras is the minimum required version
+    check_keras_version()
+
+    # optionally choose specific GPU
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    keras.backend.tensorflow_backend.set_session(get_session())
+
+    # create the model
+    print('Loading model, this may take a second...')
+    model = keras.models.load_model(args.model, custom_objects=custom_objects)
+
+    # create image data generator object
+    test_image_data_generator = keras.preprocessing.image.ImageDataGenerator()
+
+    # create a generator for testing data
+    test_generator = CSVGenerator(
+        args.test_path,
+        args.class_file,
+        args.mean_image,
+        test_image_data_generator)
+    # start collecting results
+    results = []
+    image_ids = []
+    for i in range(len(test_generator.image_names)):
+        image = test_generator.load_image(i)
+        image = test_generator.preprocess_image(image)
+        image, scale = test_generator.resize_image(image)
+
+        # run network
+        _, _, detections = model.predict_on_batch(np.expand_dims(image, axis=0))
+
+        # clip to image shape
+        detections[:, :, 0] = np.maximum(0, detections[:, :, 0])
+        detections[:, :, 1] = np.maximum(0, detections[:, :, 1])
+        detections[:, :, 2] = np.minimum(image.shape[1], detections[:, :, 2])
+        detections[:, :, 3] = np.minimum(image.shape[0], detections[:, :, 3])
+
+        # correct boxes for image scale
+        detections[0, :, :4] /= scale
+
+        # change to (x, y, w, h) (MS COCO standard)
+        detections[:, :, 2] -= detections[:, :, 0]
+        detections[:, :, 3] -= detections[:, :, 1]
+
+        # compute predicted labels and scores
+        for detection in detections[0, ...]:
+            positive_labels = np.where(detection[4:] > args.score_threshold)[0]
+
+            # append detections for each positively labeled class
+            for label in positive_labels:
+                image_result = {
+                    'image_id'    : test_generator.image_names[i],
+                    'category_id' : test_generator.label_to_name(label),
+                    'score'       : float(detection[4 + label]),
+                    'bbox'        : (detection[:4]).tolist(),
+                }
+                # append detection to results
+                results.append(image_result)
+        # append image to list of processed images
+        image_ids.append(test_generator.image_names[i])
+
+        # print progress
+        print('{}/{}'.format(i, len(test_generator.image_names)), end='\r')
+    out_name = '{}_predict_results_{}.pickle'.format(os.path.splitext(args.test_path)[0],str(args.score_threshold).split(".")[-1])
+    pickle.dump(results,open(out_name,'wb'))
---
+++ scripts/evaluate_video.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+"""
+
+Copyright 2017-2018 Fizyr (https://fizyr.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import keras
+import keras.preprocessing.image
+from keras_retinanet.models.resnet import custom_objects
+from keras_retinanet.utils.keras_version import check_keras_version
+
+import tensorflow as tf
+import numpy as np
+import argparse
+import time
+import sys
+import os
+import re
+import cv2
+import json
+import pickle
+import multiprocessing as mp
+
+def format_img(img,mean_image):
+    img = img[:, :, (2, 1, 0)]
+    img = img.astype(np.float32)
+    img[:, :, 0] -= mean_image[:,:,0]
+    img[:, :, 1] -= mean_image[:,:,1]
+    img[:, :, 2] -= mean_image[:,:,2]
+    return img
+
+def read_frames(img_path, raw_frame_queue, stop_event):
+    ok = True
+    vid = cv2.VideoCapture(img_path)
+    if cv2.__version__ >= "3.2.0":
+        vid_len = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = vid.get(cv2.CAP_PROP_FPS)
+    else:
+        vid_len = int(vid.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        fps = vid.get(cv2.cv.CV_CAP_PROP_FPS)
+    frame_num = 1
+    while ok and not stop_event.is_set():
+        if not raw_frame_queue.full():
+            ok,img = vid.read()
+            if ok:
+                raw_frame_queue.put((img, frame_num))
+                frame_num += 1
+    stop_event.set()
+    print("This thread should exit now read frames")
+    return
+
+def enqueue_frames(raw_frame_queue, processed_frame_queue, stop_event, mean_image):
+    while not stop_event.is_set():
+        try:
+            img,frame_num = raw_frame_queue.get(timeout=5)
+            X = format_img(img,mean_image)
+            processed_frame_queue.put((X,frame_num))
+        except:
+            stop_event.set()
+            print("This thread should exit now")
+    return
+
+def get_session():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    return tf.Session(config=config)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Testing script for testing video data.')
+    parser.add_argument('model', help='Path to RetinaNet model.')
+    parser.add_argument('video_path', help='Path to COCO directory (ie. /tmp/COCO).')
+    parser.add_argument('mean_image',help='Path to mean image to subtract')
+    parser.add_argument('--gpu', help='Id of the GPU to use (as reported by nvidia-smi).')
+    parser.add_argument('--score-threshold', help='Threshold on score to filter detections with (defaults to 0.7).', default=0.7, type=float)
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # parse arguments
+    args = parse_args()
+    mean_image = np.load(args.mean_image)
+    # make sure keras is the minimum required version
+    check_keras_version()
+
+    # optionally choose specific GPU
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    keras.backend.tensorflow_backend.set_session(get_session())
+
+    # create the model
+    print('Loading model, this may take a second...')
+    model = keras.models.load_model(args.model, custom_objects=custom_objects)
+
+    raw_queue = mp.Queue(50)
+    frame_queue = mp.Queue(50)
+    stop_event = mp.Event()
+    frame_stop_event = mp.Event()
+    p = mp.Process(target=read_frames, args=(args.video_path,raw_queue,stop_event))
+    p.daemon = True
+    p.start()
+    for num in range(8):
+        p = mp.Process(target=enqueue_frames, args=(raw_queue, frame_queue, frame_stop_event, mean_image))
+        p.daemon = True
+        p.start()
+    id_ = 1
+    results = []
+
+    while True:
+        st = time.time()
+        try:
+            image,frame = frame_queue.get(timeout=5)
+        except:
+            print("timed out")
+            break
+        print('Elapsed time pre-process = {}'.format(time.time() - st))
+
+        _, _, detections = model.predict_on_batch(np.expand_dims(image, axis=0))
+
+        print('Elapsed time model = {}'.format(time.time() - st))
+        # clip to image shape
+        detections[:, :, 0] = np.maximum(0, detections[:, :, 0])
+        detections[:, :, 1] = np.maximum(0, detections[:, :, 1])
+        detections[:, :, 2] = np.minimum(image.shape[1], detections[:, :, 2])
+        detections[:, :, 3] = np.minimum(image.shape[0], detections[:, :, 3])
+
+        # correct boxes for image scale
+        #detections[0, :, :4] /= scale
+
+        # change to (x, y, w, h) (MS COCO standard)
+        detections[:, :, 2] -= detections[:, :, 0]
+        detections[:, :, 3] -= detections[:, :, 1]
+
+        # compute predicted labels and scores
+        for detection in detections[0, ...]:
+            positive_labels = np.where(detection[4:] > args.score_threshold)[0]
+
+            # append detections for each positively labeled class
+            for label in positive_labels:
+                image_result = {
+                    'frame'       : frame,
+                    'category_id' : label,
+                    'scores'      : [float(det) for i,det in
+                                      enumerate(detection) if i >=4],
+                    'bbox'        : (detection[:4]).tolist(),
+                }
+
+                # append detection to results
+                results.append(image_result)
+
+    if len(results):
+        # write output
+        out_name = re.split(".mp4",args.video_path.split('/')[-1],flags=re.IGNORECASE)[0]
+        try:
+            #json.dump(results, open('{}_bbox_results.json'.format(out_name), 'w'), indent=4)
+            pickle.dump(results,open('{}_bbox_results_03.pickle'.format(out_name),'wb'))
+        except:
+            pickle.dump(results,open('default_10794_bbox_results_2.pickle','wb'))
+            #json.dump(results, open('default_bbox_results.json', 'w'), indent=4)
+
+    frame_stop_event.set()
+    stop_event.set()
+    print("Finished")
+    sys.exit()
