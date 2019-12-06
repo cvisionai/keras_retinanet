57bcfb0ddc929f320e78475ec1574f53ba67ffed
Brian Tate <brian.tate@cvisionai.com>
2019-12-05 19:11:03 -0500

    Add resizeAndFill function to support varying aspect ratio data

--- keras_retinanet/utils/image.py
+++ keras_retinanet/utils/image.py
@@ -222,3 +222,40 @@ def bb_intersection_over_union(boxA, boxB):
     iou = interArea / float(boxAArea + boxBArea - interArea)
     # return the intersection over union value
     return iou
+
+def resizeAndFill(image, desired_shape):
+    """
+    Resize an image to a desired shape (height,width) and maintaining
+    the aspect ratio. Fill any extra areas with black.
+    :param image: ndarray of the image
+    :param desired_shape: tuple describing the output shape
+
+    :returns image_resized,scaleFactor:
+    image_resized is ndarray represented the scaled + padded image
+    scaleFactor Given a pixel coordinate in the original this factor should
+    be applied to land on the new image.
+    """
+    image_height=image.shape[0]
+    image_width=image.shape[1]
+    image_channels=image.shape[2]
+
+    desired_width=desired_shape[1]
+    desired_height=desired_shape[0]
+    growth_factor=min(desired_height/image_height,
+                           desired_width/image_width)
+    new_height=int(image_height*growth_factor)
+    new_width=int(image_width*growth_factor)
+    image_resized=transform.resize(image,(new_height,new_width),anti_aliasing=True)
+    added_rows=desired_height-new_height
+    added_cols=desired_width-new_width
+
+    if added_rows:
+        black_bar=np.zeros((added_rows, new_width, image_channels))
+        image_resized = np.append(image_resized,black_bar, axis=0)
+
+    if added_cols:
+        black_bar=np.zeros((new_height, added_cols, image_channels))
+        image_resized = np.append(image_resized,black_bar, axis=1)
+
+    scaleFactor=(float(new_height)/image_height,float(new_width)/image_width)
+    return image_resized, scaleFactor
